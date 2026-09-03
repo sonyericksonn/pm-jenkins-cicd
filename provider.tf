c8f9154bc6d0ee2e163829bc61f324804a836b58
sonyericksonno <sonyericksonno@gmail>
2026-09-03 13:05:53 -0300

    feat: init

--- provider.tf
+++ provider.tf
@@ -4,15 +4,29 @@ terraform {
       source  = "Telmate/proxmox"
       version = "3.0.2-rc09"
     }
+  }
 
-    local = {
-      source  = "hashicorp/local"
-      version = "2.5.2"
+  backend "s3" {
+    bucket = "proxmox-state"
+    key    = "terraform.tfstate"
+    region = "placeholder"
+
+
+    endpoints = {
+      s3 = "http://172.18.0.2:9000"
     }
+
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    use_path_style              = true
+
+    #    locals  {
+    #      source  = "hashicorp/local"
+    #      version = "2.5.2"
   }
 }
-
-
 provider "proxmox" {
   pm_api_url = "https://192.168.0.50:8006/api2/json"
   #  username = "root@pam"
