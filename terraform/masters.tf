8d938769a4cf1d19c2c9a190b8fc7f1b2e553bfa
sonyericksonno <sonyericksonno@gmail>
2026-09-08 15:15:55 -0300

    v14.1

--- terraform/masters.tf
+++ terraform/masters.tf
@@ -65,8 +65,8 @@ resource "proxmox_vm_qemu" "masters" {
     type        = "ssh"
     user        = local.cloud_init.user
     private_key = file("id_rsa")
- 
-#    private_key = file("/home/russo/.ssh/id_rsa")
+
+    #    private_key = file("/home/russo/.ssh/id_rsa")
     host = cidrhost(
       local.cdir,
       local.masters.network_last_octect + count.index
