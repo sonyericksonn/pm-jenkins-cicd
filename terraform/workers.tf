2afebcb1ecee62d70501e5ab1a840844a993e389
sonyericksonno <sonyericksonno@gmail>
2026-09-09 20:23:17 -0300

    v27.1

--- terraform/workers.tf
+++ terraform/workers.tf
@@ -1,8 +1,3 @@
-# Adicione esta declaração no seu variables.tf ou topo do arquivo
-variable "ssh_private_key" {
-  type      = string
-  sensitive = true
-}
 
 resource "proxmox_vm_qemu" "workers" {
   count = local.workers.count
