5b5bd1b9dbd01361aaa0906f7ff68aa4d668e359
sonyericksonno <sonyericksonno@gmail>
2026-09-15 14:57:36 -0300

    v44.2

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -10,8 +10,6 @@ terraform {
       key    = "terraform.tfstate"
       region = "placeholder"
       endpoints = "http://172.18.0.3:9000"
-      }
-  
       skip_credentials_validation = true
       skip_metadata_api_check     = true
       skip_region_validation      = true
