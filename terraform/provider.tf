6fd5cb6f0f130dcf0a4347b0b1790d440599d981
sonyericksonno <sonyericksonno@gmail>
2026-09-15 15:21:36 -0300

    v46

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,31 +1,32 @@
-# CORRETO ✅
 terraform {
-  required_version = ">= 1.0.0"
-
   required_providers {
     proxmox = {
       source  = "Telmate/proxmox"
-      version = "2.9.14"
+      version = "3.0.1-rc6"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.5.2"
     }
   }
-
-  # O backend fica aqui, no mesmo nível do required_providers
   backend "s3" {
-    bucket                      = "proxmox-state"
-    key                         = "terraform.tfstate"
-    region                      = "us-east-1"
-    endpoint                    = "http://172.18.0.3:9000" # ajuste conforme seu ambiente
+    bucket = "proxmox-state"
+    key    = "terraform.tfstate"
+    region = "placeholder"
+
+    endpoints = {
+      s3 = "http://192.168.96.2:9000"
+    }
+
     skip_credentials_validation = true
+    skip_metadata_api_check     = true
     skip_region_validation      = true
-    force_path_style            = true
+    skip_requesting_account_id  = true
+    use_path_style              = true
   }
 }
+
 provider "proxmox" {
-  pm_api_url = "https://192.168.0.50:8006/api2/json"
-  #  username = "root@pam"
-  #  password = "proxmox1"
+  pm_api_url      = "https://192.168.0.2:8006/api2/json"
   pm_tls_insecure = true
 }
-
-
-
