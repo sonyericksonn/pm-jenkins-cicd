dca9dbecddb9011efa4497220683fe1ca150c183
sonyericksonno <sonyericksonno@gmail>
2026-09-16 19:32:06 -0300

    feat: key rsa

--- terraform/workers.tf
+++ terraform/workers.tf
@@ -64,7 +64,7 @@ resource "proxmox_vm_qemu" "workers" {
   connection {
     type        = "ssh"
     user        = local.cloud_init.user
-    private_key = file("${path.module}/id_rsa")
+    private_key = file("${path.cwd}/id_rsa")
     host = cidrhost(
       local.cidr,
       local.workers.network_last_octect + count.index
